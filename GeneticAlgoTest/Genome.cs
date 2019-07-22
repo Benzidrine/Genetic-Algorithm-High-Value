9291e07ef69a7ecd7d26cf2e2fa3570276cc624d
benzidrine <benzidrine@yahoo.com>
2019-07-22 18:20:48 +0800

    first commit

---
+++ GeneticAlgoTest/Genome.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeneticAlgoTest
+{
+    public class Genome
+    {
+        public Genome(int id)
+        {
+            ID = id;
+            this.Genes = new List<Boolean>();
+            for (int i = 0; i < 6; i++)
+            {
+                Random rnd = new Random();
+                bool Gene = false;
+                int Decision = rnd.Next(0, 2);
+                if (Decision == 1) Gene = !Gene;
+                this.Genes.Add(Gene);
+            }
+        }
+
+        public Genome(Genome Father, Genome Mother, bool XY, int id)
+        {
+            ID = id;
+            this.Genes = new List<Boolean>();
+
+            //If XY is true then spawn first three of father and last three of mother otherwise do the opposite
+            if (XY)
+            {
+                //Father
+                for (int i = 0; i < 3; i++)
+                {
+                    Genes.Add(Father.Genes[i]);
+                }
+                //Mother
+                for (int i = 3; i < 6; i++)
+                {
+                    Genes.Add(Mother.Genes[i]);
+                }
+            }
+            else
+            {
+                //Mother
+                for (int i = 0; i < 3; i++)
+                {
+                    Genes.Add(Mother.Genes[i]);
+                }
+                //Father
+                for (int i = 3; i < 6; i++)
+                {
+                    Genes.Add(Father.Genes[i]);
+                }
+            }
+        }
+
+        public int ID { get; set; }
+        public List<Boolean> Genes { get; set; }
+        public int TotalValue { get; set; }
+        public double SurvivalChance { get; set; }
+
+        public Genome Clone()
+        {
+            Genome newGenome = new Genome(ID);
+            newGenome.Genes = new List<bool>();
+            newGenome.Genes = Genes;
+            newGenome.TotalValue = TotalValue;
+            newGenome.SurvivalChance = SurvivalChance;
+            return newGenome;
+        }
+
+        public int Total()
+        {
+            int _total = 0;
+            foreach(Boolean b in Genes)
+            {
+                if (b)
+                {
+                    _total++;
+                }
+            }
+            TotalValue = _total;
+            return _total;
+        }
+
+        public double SquaredTotal()
+        {
+            return ((double)TotalValue * (double)TotalValue);
+        }
+
+        public string GeneReadout()
+        {
+            string output = "[";
+            foreach (Boolean b in Genes)
+            {
+                output += b ? " 1" : " 0";
+            }
+            output += " ]";
+            return output;
+        }
+
+        //Chance to be included
+        public void DetermineSurvivalChance(double SummedTotal)
+        {
+            SurvivalChance = ((double)SquaredTotal() / (double)SummedTotal);
+        }
+
+        //Put the first half at the front and the second half at the back of the genes list
+        public void Crossover(Genome genome, int StartingIndex)
+        {
+            Genome NewGenome = new Genome(genome.ID);
+            NewGenome.Genes = new List<Boolean>();
+
+            if (this.Genes.Count == genome.Genes.Count)
+            {
+                for (int i = 0; i < genome.Genes.Count; i++)
+                {
+                    if (i < (genome.Genes.Count / 2))
+                        NewGenome.Genes.Add(genome.Genes[(i + (genome.Genes.Count / 2))]);
+                    else
+                        NewGenome.Genes.Add(genome.Genes[(i - (genome.Genes.Count / 2))]);
+                }
+            }
+
+            genome.Genes = NewGenome.Genes;
+        }
+
+        public void Mutate()
+        {
+            Random rnd = new Random();
+            int MutationPoint = rnd.Next(0, Genes.Count);
+            Genes[MutationPoint] = !Genes[MutationPoint];
+        }
+    }
+}
---
+++ GeneticAlgoTest/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GeneticAlgoTest
+{
+    class Program
+    {
+        private static int GlobalID = 0;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Genetic Algorithm Example:");
+            Console.WriteLine("Attempting to get to value six per Gene");
+            Console.WriteLine("Epochs (Integer Value):");
+            int Epoches = 0;
+            string EpochStr = Console.ReadLine();
+            if (int.TryParse(EpochStr, out Epoches))
+            {
+                Console.WriteLine(Epoches + " Epochs");
+                Console.WriteLine("Crossover Rate (Decimal Value):");
+                double CrossoverRate = 0;
+                string CrossoverRateStr = Console.ReadLine();
+                if (double.TryParse(CrossoverRateStr, out CrossoverRate))
+                {
+                    Console.WriteLine(CrossoverRate + " Crossover Rate");
+                    Console.WriteLine("Mutation Rate (Decimal Value):");
+                    double MutationRate = 0;
+                    string MutationRateStr = Console.ReadLine();
+                    if (double.TryParse(MutationRateStr, out MutationRate))
+                    {
+                        Console.WriteLine(MutationRate + " Mutation Rate");
+                        Console.WriteLine("Running");
+                        Console.WriteLine();
+
+                        List<Genome> genomes = new List<Genome>();
+                        for (int i = 0; i < Epoches; i++)
+                        {
+                            //Build list of genomes takign into account any survivors
+                            for (int j = genomes.Count; j < 4; j++)
+                            {
+                                //Make children
+                                bool XY = (j % 2 == 0);
+                                if (genomes.Count >= 2)
+                                    genomes.Add(new Genome(genomes[0], genomes[1], XY, GlobalID++));
+                                else //Create Initial Parents
+                                    genomes.Add(new Genome(GlobalID++));
+                            }
+                            Console.WriteLine("Epoch " + (i + 1).ToString());
+                            double SummedTotal = 0;
+                            foreach (Genome genome in genomes)
+                            { 
+                                Console.WriteLine(genome.GeneReadout() + " " + genome.Total());
+                                SummedTotal += genome.SquaredTotal();
+                            }
+                            foreach (Genome genome in genomes)
+                            {
+                                genome.DetermineSurvivalChance(SummedTotal);
+                            }
+                            //Survived Genomes
+                            List<Genome> SurvivedGenomes = new List<Genome>();
+                            Random rnd = new Random();
+                            double Chance = rnd.NextDouble();
+                            double lastChance = 0;
+                            //Add genomes to next epoch based on their survival chance
+                            foreach (Genome genome in genomes)
+                            {
+                                if (Chance < (genome.SurvivalChance + lastChance))
+                                {
+                                    SurvivedGenomes.Add(genome.Clone());
+                                    genome.ID = -1;
+                                    break;
+                                }
+                                lastChance += genome.SurvivalChance;
+                            }
+                            //Remove Surviving Genome
+                            genomes.RemoveAll(g => g.ID == -1);
+
+                            Chance = rnd.NextDouble();
+                            //Add genomes to next epoch based on their survival chance - 2nd
+                            SummedTotal = 0;
+                            foreach (Genome genome in genomes)
+                            {
+                                SummedTotal += genome.SquaredTotal();
+                            }
+                            foreach (Genome genome in genomes)
+                            {
+                                genome.DetermineSurvivalChance(SummedTotal);
+                            }
+                            lastChance = 0;
+                            foreach (Genome genome in genomes)
+                            {
+                                if (Chance < (genome.SurvivalChance + lastChance))
+                                {
+                                    SurvivedGenomes.Add(genome.Clone());
+                                    break;
+                                }
+                                lastChance += genome.SurvivalChance;
+                            }
+                            //Mutate Genomes based on mutation rate
+                            foreach (Genome genome in SurvivedGenomes)
+                            {
+                                Chance = rnd.NextDouble();
+                                if (Chance < MutationRate)
+                                {
+                                    genome.Mutate();
+                                }
+                            }
+                            //Mutate Genomes based on mutation rate
+                            foreach (Genome genome in SurvivedGenomes)
+                            {
+                                Chance = rnd.NextDouble();
+                                if (Chance < CrossoverRate)
+                                {
+                                    genome.Crossover(genome,3);
+                                }
+                            }
+                            //Report Survivors
+                            Console.WriteLine("Survivors:");
+                            foreach (Genome genome in SurvivedGenomes)
+                            {
+                                Console.WriteLine(genome.GeneReadout() + " Total: " + genome.Total() + " ID: " + genome.ID + " Survival Chance: " + genome.SurvivalChance);
+                            }
+
+                            genomes = SurvivedGenomes;
+                        }
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
